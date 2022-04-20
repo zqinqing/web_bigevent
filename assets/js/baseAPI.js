d640d80172810d95bed1400ef3ffe120895083e0
itlz <qinqingz@foxmail.com>
2022-04-20 16:39:19 +0800

    更新了首台首页的跳转链接,以及新增了本地的数据库地址

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,7 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的Ajax请求之前，统一拼接请求的路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // options.url = 'http://127.0.0.1:3007' + options.url;
     // console.log(options.url);
 
     // 先进行判断，只有需要权限的请求接口 才进行请求头的设置
